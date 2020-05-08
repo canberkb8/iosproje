2d1d99adbcbe176db824ca66462321c88559c154
canberkb8 <canberkbibican@hotmail.com>
2020-05-08 03:57:43 -0700

    first commit

---
+++ iosproje/ContentView.swift
@@ -0,0 +1,49 @@
+//
+//  ContentView.swift
+//  iosproje
+//
+//  Created by Canberk BİBİCAN on 24.04.2020.
+//  Copyright © 2020 Canberk BİBİCAN. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        FirstPage()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct FirstPage : View {
+    
+    @State var no = ""
+    
+    var body : some View{
+        VStack(spacing: 20){
+            Image("pic")
+            
+            Text("Verify Your Number").font(.largeTitle).fontWeight(.heavy)
+            
+            Text("Please Enter Your Number To Verify Your Account").font(.body).foregroundColor(.gray).padding(.top, 12)
+            
+            TextField("Number", text: $no)
+            .padding()
+            .background(Color("Color"))
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            
+            Button(action:{
+                
+            }) {
+                
+                Text("Send").frame(width: UIScreen.main.bounds.width - 30, height: 50)
+                
+                }.foregroundColor(.white).background(Color.orange).cornerRadius(10)
+        }
+    }
+}
