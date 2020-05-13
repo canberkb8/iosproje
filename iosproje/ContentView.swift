63bb4fea126312a5da9cf2043febcdc24a598230
alicantozlu <alicantozlu@gmail.com>
2020-05-13 11:21:48 -0700

    6

--- iosproje/ContentView.swift
+++ iosproje/ContentView.swift
@@ -1,4 +1,4 @@
-//
+
 //  ContentView.swift
 //  iosproje
 //
@@ -10,13 +10,16 @@ import SwiftUI
 import Firebase
 import SDWebImageSwiftUI
 
+// ---------------------------------------------- CONTENT VIEW
+// TOP NAVIGATION BAR
+
 struct ContentView: View {
     var body: some View {
         
         TabView{
             
             NavigationView{
-            
+                
                 Home()
                     .navigationBarTitle("Instagram")
                     .navigationBarItems(leading: Button(action: {
@@ -26,9 +29,9 @@ struct ContentView: View {
                         Image("cam").resizable().frame(width: 30, height: 30)
                         
                     })
-                    .foregroundColor(Color("darkAndWhite"))
-                    , trailing:
-                
+                        .foregroundColor(Color("darkAndWhite"))
+                        , trailing:
+                        
                         HStack{
                             
                             Button(action: {
@@ -47,9 +50,9 @@ struct ContentView: View {
                             }
                             .foregroundColor(Color("darkAndWhite"))
                         }
-                
+                        
                 )
-
+// BOTTOM NAVIGATION BAR
             }.tabItem {
                 
                 Image("home")
@@ -87,22 +90,23 @@ struct ContentView_Previews: PreviewProvider {
 
 
 
-
+// ---------------------------------------------- HOME VIEW
 
 struct Home : View {
     
-     @ObservedObject var observed = observer()
-     @State var show = false
-     @State var user = ""
-     @State var url = ""
+    @ObservedObject var observed = observer()
+    @ObservedObject var postsobserver = Postsobserver()
+    @State var show = false
+    @State var user = ""
+    @State var url = ""
     
     var body : some View{
-    
-    
-    ScrollView(.vertical, showsIndicators: false) {
         
-        VStack{
         
+        ScrollView(.vertical, showsIndicators: false) {
+            
+            VStack{
+                
                 //Ust tarafta gorulen profil resimleri
                 ScrollView(.horizontal, showsIndicators: false) {
                     
@@ -114,17 +118,21 @@ struct Home : View {
                         }
                         
                     }
-            }
-                
-                ForEach(0..<8){_ in
-                
-                    postCard(user: "",image: "", id: "")
-                
+                }
+                if postsobserver.posts.isEmpty{
+                    
+                    Text("No Posts").fontWeight(.heavy)
+                }
+                else{
+                ForEach(postsobserver.posts){i in
+                    
+                    postCard(user: i.name, image: i.image, image1: i.image1, id: i.id, likes: i.likes, comments: i.comments)
+                    }
                 }
             }
-          }.sheet(isPresented: $show) {
-                     
-                     statusView(url: self.url,name: self.user)
+        }.sheet(isPresented: $show) {
+            
+            statusView(url: self.url,name: self.user)
         }
     }
 }
@@ -133,8 +141,8 @@ struct Home : View {
 
 
 
-
-//Profil resminin cercevesi
+// ---------------------------------------------- STATUSCARD VIEW
+//Story resim cerceveleri
 
 struct StatusCard : View{
     var imName = ""
@@ -146,17 +154,17 @@ struct StatusCard : View{
     var body : some View{
         
         VStack{
-
-             AnimatedImage(url: URL(string: imName))
-                 .resizable()
-                 .frame(width: 80 , height: 80)
-                 .clipShape(Circle())
-                 .onTapGesture {
-             
-                    self.user1=self.user
-                     self.url = self.imName
-                     self.show.toggle()
             
+            AnimatedImage(url: URL(string: imName))
+                .resizable()
+                .frame(width: 80 , height: 80)
+                .clipShape(Circle())
+                .onTapGesture {
+                    
+                    self.user1=self.user
+                    self.url = self.imName
+                    self.show.toggle()
+                    
             }
             Text(user).fontWeight(.light)
         }
@@ -164,112 +172,199 @@ struct StatusCard : View{
 }
 
 
-
-
-struct postCard : View{
+// ---------------------------------------------- POSTCARD VIEW
+// POST BOTTOM BUTTONS AND STATS
+struct postCard : View {
     
     var user = ""
     var image = ""
+    var image1 = ""
     var id = ""
     var likes = ""
     var comments = ""
-
     
     var body : some View{
         
         VStack(alignment: .leading, content: {
-            
+ 
             HStack{
                 
-                Image("testing").resizable().frame(width: 30, height: 30).clipShape(Circle())
-                Text("User")
+                AnimatedImage(url: URL(string: image1)).resizable().frame(width: 30, height: 30).clipShape(Circle())
+                Text(user)
                 Spacer()
-                Button(action :{
+                Button(action: {
                     
-                }){
+                }) {
+                    
                     Image("menu").resizable().frame(width: 15, height: 15)
                 }.foregroundColor(Color("darkAndWhite"))
             }
-            Image("testing").resizable().frame(height: 350)
+            
+            AnimatedImage(url: URL(string: image)).resizable().frame(height: 350)
             
             HStack{
                 
-                Button(action :{
+                Button(action: {
                     
-                }){
+                }) {
+                    
                     Image("comment").resizable().frame(width: 26, height: 26)
                 }.foregroundColor(Color("darkAndWhite"))
                 
-                Button(action :{
+                Button(action: {
                     
-                }){
+                    // update likes...
+                    
+                    let db = Firestore.firestore()
+                   
+                    let like = Int.init(self.likes)!
+                    db.collection("posts").document(self.id).updateData(["likes": "\(like + 1)"]) { (err) in
+                        
+                        if err != nil{
+                            
+                            print((err))
+                            return
+                        }
+                        
+                        print("updated....")
+                    }
+                    
+                }) {
+                    
                     Image("heart").resizable().frame(width: 26, height: 26)
                 }.foregroundColor(Color("darkAndWhite"))
                 
                 Spacer()
                 
-                Button(action :{
+                Button(action: {
                     
-                }){
+                }) {
+                    
                     Image("saved").resizable().frame(width: 30, height: 30)
                 }.foregroundColor(Color("darkAndWhite"))
                 
             }.padding(.top, 8)
             
-            Text("2 Likes").padding(.top, 8)
-            Text("View all 3 Comments")
             
-        }).padding(8)
+            Text("\(likes) Likes").padding(.top, 8)
+            Text("View all \(comments) Comments")
+            
+         }).padding(8)
     }
 }
 
-
-//Firebaseden profil resmi cekme
+// ---------------------------------------------- OBSERVER
+// Firebase cloud storage baglantisi resim kontrolleri
 
 class observer : ObservableObject{
-
-@Published var status = [datatype]()
-
-init() {
     
-    let db = Firestore.firestore()
-    db.collection("status").addSnapshotListener { (snap, err) in
+    @Published var status = [datatype]()
+    
+    init() {
         
-        if err != nil{
+        let db = Firestore.firestore()
+        db.collection("status").addSnapshotListener { (snap, err) in
             
-            print((err?.localizedDescription)!)
-            return
-        }
-        
-        for i in snap!.documentChanges{
-            
-            if i.type == .added{
+            if err != nil{
                 
-                let id = i.document.documentID
-                let name = i.document.get("name") as! String
-                let image = i.document.get("image") as! String
-                
-                self.status.append(datatype(id: id, name: name, image: image))
+                print((err?.localizedDescription)!)
+                return
             }
             
-            if i.type == .removed{
-                               
-                               let id = i.document.documentID
-                               
-                               
-                               for j in 0..<self.status.count{
-                                   
-                                   if self.status[j].id == id{
-                                       
-                                       self.status.remove(at: j)
-                                       return
-                                   }
-                               }
-                           }
+            for i in snap!.documentChanges{
+                
+                if i.type == .added{
+                    
+                    let id = i.document.documentID
+                    let name = i.document.get("name") as! String
+                    let image = i.document.get("image") as! String
+                    
+                    self.status.append(datatype(id: id, name: name, image: image))
+                }
+                
+                if i.type == .removed{
+                    
+                    let id = i.document.documentID
+                    
+                    
+                    for j in 0..<self.status.count{
+                        
+                        if self.status[j].id == id{
+                            
+                            self.status.remove(at: j)
+                            return
+                        }
+                    }
+                }
             }
         }
     }
 }
+
+// ---------------------------------------------- POSTOBSERVER
+
+class Postsobserver : ObservableObject{
+    
+    @Published var posts = [datatype1]()
+    
+    init() {
+        
+        let db = Firestore.firestore()
+        db.collection("posts").addSnapshotListener { (snap, err) in
+            
+            if err != nil{
+                
+                print((err?.localizedDescription)!)
+                return
+            }
+            
+            for i in snap!.documentChanges{
+                
+                if i.type == .added{
+                    //firebase icine yazilan alanlar
+                    let id = i.document.documentID
+                    let name = i.document.get("name") as! String
+                    let image = i.document.get("image") as! String
+                    let image1 = i.document.get("image1") as! String
+                    let comment = i.document.get("comments") as! String
+                    let likes = i.document.get("likes") as! String
+                    
+                    self.posts.append(datatype1(id: id, name: name, image: image, image1: image1, comments: comment, likes: likes))
+                }
+                
+                if i.type == .removed{
+                    
+                    let id = i.document.documentID
+                    
+                    
+                    for j in 0..<self.posts.count{
+                        
+                        if self.posts[j].id == id{
+                            
+                            self.posts.remove(at: j)
+                            return
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+// ----------------------------------------------
+
+struct datatype1 : Identifiable {
+    
+    var id : String
+    var name : String
+    var image : String
+    var image1 : String
+    var comments : String
+    var likes : String
+}
+
+// ----------------------------------------------
+
+// ----------------------------------------------
 
 struct datatype : Identifiable {
     
@@ -278,6 +373,8 @@ struct datatype : Identifiable {
     var image : String
 }
 
+// ----------------------------------------------
+// Story resimleri tiklama ile genisleme ayari
 struct statusView : View {
     
     var url = ""
@@ -302,3 +399,4 @@ struct statusView : View {
         
     }
 }
+
